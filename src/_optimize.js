49606046ab637647fbc74fefb5dd388b210e00cf
Montmorency <lamberh@tcd.ie>
2025-11-27 12:20:45 +0000

    Update _optimize.js
    Increasing min width

--- src/_optimize.js
+++ src/_optimize.js
@@ -3,7 +3,7 @@ import { nest } from 'd3-collection';
 
 import { cssAboveClass, cssBelowClass, cssLastClass } from './_css';
 
-const LABEL_MIN_WIDTH = { horizontal: 60, vertical: 30 };
+const LABEL_MIN_WIDTH = { horizontal: 80, vertical: 30 };
 const ADJUST_PIXEL_STEP = 25;
 const DEBUG_CHART = false;
 const DEBUG_TIME = true;
